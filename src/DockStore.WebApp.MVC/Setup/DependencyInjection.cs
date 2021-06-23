111bcebd6b61ad767ed5b5aa65c938a2ff5dcc29
EDockhorn <johnnyerick111@hotmail.com>
2021-06-22 23:50:22 -0300

    Add WebApp project.

---
+++ src/DockStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using DockStore.Catalogo.Application.Services;
+using DockStore.Catalogo.Data;
+using DockStore.Catalogo.Data.Repository;
+using DockStore.Catalogo.Domain;
+using DockStore.Core.Bus;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DockStore.WebApp.MVC.Setup
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            // Domain Bus (Mediator)
+            services.AddScoped<IMediatrHandler, MediatrHandler>();
+
+            // Catalogo
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IProdutoAppService, ProdutoAppService>();
+            services.AddScoped<IEstoqueService, EstoqueService>();
+            services.AddScoped<CatalogoContext>();
+
+            //services.AddScoped<INotificationHandler<ProdutoAbaixoEstoqueEvent>, ProdutoEventHandler>();
+        }
+    }
+}
